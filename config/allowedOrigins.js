b983575af750cb6ad4466b8d3b6baa27a826f0e1
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2023-07-23 14:38:37 +0700

    Add Database link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://technotes-fe-lilac.vercel.app'
+    'https://technotes-fe-lilac.vercel.app',
+    'https://good-pink-adder-gear.cyclic.app'
 ];
 
 module.exports = allowedOrigins;
